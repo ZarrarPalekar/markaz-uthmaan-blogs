b06d77e12bbc55afe22073541dfea4523eff8535
Zarrar Palekar <zarrar.palekar@azularc.com>
2024-04-01 11:51:01 +0530

    Navbar logo responsive

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export default function Navbar() {
   return (
     <nav className="w-full relative flex items-center justify-between max-w-2xl mx-auto px-4 py-5">
-      <Link href="/" className="font-bold text-3xl flex items-center">
+      <Link href="/" className="font-bold lg:text-3xl flex items-center">
         <Image src={"/logo.png"} alt="image" width={50} height={50} />
         <span className="ml-2">Markaz</span>
         <span className="text-primary">Uthmaan</span>
